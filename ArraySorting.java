88448488d69966ddb162438a21cecfcef593bc48
shaikova <alenashaikova@gmail.com>
2023-11-16 22:52:20 +0100

    fix1

--- ArraySorting.java
+++ ArraySorting.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class ArraySorting {
         public static void main(String[] args) {
             int[] array = new int[args.length];
-
+//ghj
             for (int i = 0; i < args.length; i++) {
                     array[i] = Integer.parseInt(args[i]);
             }
